6615fab07ba89fd78c98fe60fcd8e486ee256017
andiwand <stefl.andreas@gmail.com>
2014-11-02 19:23:00 +0100

    improve WifiSelector

--- src/at/stefl/smartwifi/WifiSelector.java
+++ src/at/stefl/smartwifi/WifiSelector.java
@@ -1,77 +1,129 @@
 package at.stefl.smartwifi;
 
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import android.net.wifi.ScanResult;
-import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 
 public class WifiSelector {
 
-	private static class WifiData {
-		WifiConfiguration configuration;
-		ScanResult lastScan;
-
-		@Override
-		public String toString() {
-			return configuration.toString() + "; " + lastScan.toString();
-		}
-	}
-
-	private static List<WifiData> merge(List<WifiConfiguration> configurations,
-			List<ScanResult> scans) {
-		List<WifiData> result = new LinkedList<WifiData>();
-
-		for (WifiConfiguration configuration : configurations) {
-			String ssid = WifiConfigurationUtil.getSsid(configuration);
-			for (ScanResult scan : scans) {
-				if (scan.BSSID.equals(configuration.BSSID)
-						|| ssid.equals(scan.SSID)) {
-					WifiData data = new WifiData();
-					data.configuration = configuration;
-					data.lastScan = scan;
-					result.add(data);
-				}
-			}
-		}
-
-		return result;
+	private static class ScanData {
+		int level;
+		long timestamp;
 	}
 
 	private WifiManager wifiManager;
 
-	public WifiSelector() {
+	// TODO: use scan count?
+	private double scanCacheTime;
 
+	private final Deque<Long> lastScans;
+	private final Map<String, Deque<ScanData>> lastBssidScans;
+	// TODO: free memory
+	private final Map<String, Set<String>> bssidToSsids;
+
+	public WifiSelector() {
+		this.lastScans = new LinkedList<Long>();
+		this.lastBssidScans = new HashMap<String, Deque<ScanData>>();
+		this.bssidToSsids = new HashMap<String, Set<String>>();
 	}
 
 	public void init(WifiManager wifiManager) {
 		this.wifiManager = wifiManager;
 	}
 
-	public void reportScan(List<ScanResult> scans) {
+	private void addScan(ScanResult scan) {
+		long timestamp = System.nanoTime();
+		lastScans.add(timestamp);
 
+		Deque<ScanData> scans = lastBssidScans.get(scan.BSSID);
+		if (scans == null) {
+			scans = new LinkedList<ScanData>();
+			lastBssidScans.put(scan.BSSID, scans);
+		}
+
+		ScanData scanData = new ScanData();
+		scanData.timestamp = timestamp;
+		scanData.level = scan.level;
+		scans.add(scanData);
+
+		Set<String> bssids = bssidToSsids.get(scan.SSID);
+		if (bssids == null) {
+			bssids = new HashSet<String>();
+			bssidToSsids.put(scan.SSID, bssids);
+		}
+
+		bssids.add(scan.BSSID);
+	}
+
+	private void filterScans() {
+		long timestamp = System.nanoTime();
+
+		Iterator<Long> i = lastScans.iterator();
+		while (i.hasNext()) {
+			double time = (timestamp - i.next()) / 1000000000d;
+			if (time <= scanCacheTime)
+				break;
+			i.remove();
+		}
+
+		Iterator<Entry<String, Deque<ScanData>>> j = lastBssidScans.entrySet()
+				.iterator();
+		while (j.hasNext()) {
+			Entry<String, Deque<ScanData>> entry = j.next();
+			Deque<ScanData> scans = entry.getValue();
+
+			Iterator<ScanData> k = scans.iterator();
+			while (k.hasNext()) {
+				ScanData scan = k.next();
+				double time = (timestamp - scan.timestamp) / 1000000000d;
+				if (time <= scanCacheTime)
+					break;
+				k.remove();
+			}
+
+			if (scans.isEmpty())
+				j.remove();
+		}
+	}
+
+	private Map<String, Double> getCurrentScores() {
+		Map<String, Double> result = new HashMap<String, Double>();
+
+		for (Entry<String, Deque<ScanData>> entry : lastBssidScans.entrySet()) {
+			double score = 0;
+
+			for (ScanData scan : entry.getValue()) {
+				double level = WifiManager
+						.calculateSignalLevel(scan.level, 101) / 100d;
+				score += level;
+			}
+
+			score /= lastScans.size();
+			result.put(entry.getKey(), score);
+		}
+
+		return result;
+	}
+
+	public void reportScan(List<ScanResult> scans) {
+		for (ScanResult scan : scans) {
+			addScan(scan);
+		}
+
+		filterScans();
 	}
 
 	public void select() {
-		List<WifiConfiguration> configurations = wifiManager
-				.getConfiguredNetworks();
-		List<ScanResult> scans = wifiManager.getScanResults();
-		List<WifiData> data = merge(configurations, scans);
 
-		WifiData best = null;
-		for (WifiData d : data) {
-			if ((best == null) || (d.lastScan.level > best.lastScan.level)) {
-				best = d;
-			}
-		}
-
-		if (best == null) {
-			return;
-		}
-
-		wifiManager.disconnect();
-		wifiManager.enableNetwork(best.configuration.networkId, true);
 	}
 
 }
